36a5889d81bf6e1a358c4833f411a1d832fc22dc
fuzao-jbl <108827743+fuzao-jbl@users.noreply.github.com>
2026-08-26 15:44:58 -0300

    Update and rename untitled.m to lab4_matlab.m

--- lab4_matlab.m
+++ lab4_matlab.m
@@ -15,19 +15,6 @@ G = K * tf(wn^2, [1 2*zeta*wn wn^2]);
 
 Gmf = feedback(K*G, 1);
 
-% Largura de banda
-wb = bandwidth(G);
-fb = wb/(2*pi);
-
-% Frequencia de amostragem
-fs = 10 * fb;
-T0 = 1/fs;
-
-% Funcao de transferencia discreta (zero holder)
-Gz = c2d(G, T0, 'zoh');
-
-Gmfd = feedback(K*Gz, 1);
-
 %----------------------------
 % exercicio 3
 % ---------------------------
@@ -82,7 +69,7 @@ disp('frequencia Hz malha fechada')
 disp(fsf);
 
 % ----------------------------------------
-% exercicio 9
+% exercicio 9 e 10
 %------------------------------------------
 figure
 plot(out.y_c.Time, out.y_c.Data, 'b')
@@ -110,3 +97,75 @@ grid
 hold on;
 stairs(out.u_d.Time, out.u_d.Data, 'r');
 legend('Contínuo', 'Discreto')
+
+% ---------------------------------------
+% exercicio 12
+% ------------------------------------------------
+
+figure
+rlocus(G)
+grid on
+title('Lugar das raízes do sistema contínuo de malha fechada')
+
+% ------------------------------------------
+% exercicio 14
+% ------------------------------------------
+
+figure
+bode(G)
+margin(G)
+grid on
+title('Diagrama de Bode com margens de ganho e fase')
+
+%------------------------------------------------------
+% exercicio 16
+% -------------------------------------------------------
+
+T0 = T0f;
+
+Gz = c2d(G, T0, 'zoh');
+Gmfz = feedback(K*Gz, 1);
+
+disp('Planta discreta Gz')
+Gz
+
+disp('Malha fechada discreta Gmfz')
+Gmfz
+
+% ----------------------------------------
+% EXERCICIO 17
+% ----------------------------------------
+
+polos_mf_cont = pole(Gmf);
+zeros_mf_cont = zero(Gmf);
+
+polos_mf_disc = pole(Gmfz);
+zeros_mf_disc = zero(Gmfz);
+
+disp('Polos da malha fechada continua:')
+disp(polos_mf_cont)
+
+disp('Zeros da malha fechada continua:')
+disp(zeros_mf_cont)
+
+disp('Polos da malha fechada discreta:')
+disp(polos_mf_disc)
+
+disp('Zeros da malha fechada discreta:')
+disp(zeros_mf_disc)
+
+% ----------------------------------------
+% EXERCICIO 18
+% ----------------------------------------
+
+figure
+
+step(R*Gmf, 24)
+hold on
+step(R*Gmfz, 24)
+
+grid on
+title('Resposta ao degrau da malha fechada')
+xlabel('Tempo (s)')
+ylabel('Saída y')
+legend('Contínuo', 'Discreto', 'Location', 'best')
